2e90b09d11aaa75124279c8d06e9e743969fef33
chbachman <cbachman@bachmangroup.com>
2016-09-16 23:55:07 -0500

    Per Page JS Loading implemented.

--- src/app/reading.js
+++ src/app/reading.js
@@ -1,4 +1,61 @@
+/* global PDFJS */
 angular.module('app')
 .component('reading', {
 	templateUrl: 'app/reading.html'
+})
+.controller('pdf', function ($rootScope) {
+	console.log('Controller');
+
+	$rootScope.$on('externalLoad', function () {
+		PDFJS.workerSrc = '/vendor/pdf.worker.js';
+
+		PDFJS.getDocument('/images/reading.pdf').then(function (pdf) {
+			// Fetch the page.
+			pdf.getPage(2).then(function (page) {
+				renderPage(page);
+
+				page.getTextContent({combineTextItems: true, normalizeWhitespace: true}).then(function (content) {
+					var totalText = '';
+					var item;
+					for (item in content.items) {
+						if ({}.hasOwnProperty.call(content.items, item)) {
+							var text = content.items[item].str.trim();
+
+							if (text.trim().length === 0) {
+								continue;
+							}
+
+							totalText += (text + '\n');
+						}
+					}
+
+					console.log(totalText);
+				});
+			});
+		});
+	});
+
+	function renderPage(page) {
+		var viewport = page.getViewport(1);
+
+		var scale = (2000 - 30) / viewport.width;
+
+		viewport = page.getViewport(scale);
+
+		// Prepare canvas using PDF page dimensions.
+		var canvas = document.getElementById('the-canvas');
+		var context = canvas.getContext('2d');
+		canvas.height = viewport.height;
+		canvas.width = viewport.width;
+
+		// Render PDF page into canvas context.
+		var renderContext = {
+			canvasContext: context,
+			viewport: viewport
+		};
+		page.render(renderContext);
+	}
+})
+.directive('pdfjs', function () {
+
 });
--- src/routes.js
+++ src/routes.js
@@ -34,18 +34,32 @@ angular.module('app')
 
 	$transitionsProvider.onEnter({to: '*'},
 		function ($transition, $state) {
-			_.forEach($state.js, function (value, key) {
-				if (!document.getElementById($state.component + key)) {
-					var script = document.createElement('script');
-					script.id = $state.component + key;
-					script.type = 'text/javascript';
-					script.async = true;
-					script.onload = function () {
-						console.log('Loaded');
-					};
-					script.src = value;
-					document.getElementsByTagName('head')[0].appendChild(script);
+			function checkForEvent() {
+				if (_.size($state.js) === 0) { // We are empty, and don't have any more dependencies to load.
+					// This is a really ugly way to send an event with $rootScope without having it. Sorry :/
+					var $body = angular.element(document.body);
+					var $rootScope = $body.injector().get('$rootScope');
+					$rootScope.$broadcast('externalLoad');
 				}
+			}
+
+			// Check to see if we are already satisified.
+			checkForEvent();
+
+			_.forEach($state.js, function (value) {
+				var script = angular.element('<script></script>');
+				script.attr({
+					type: 'text/javascript',
+					src: value
+				});
+				angular.element(document.head).append(script);
+
+				script.ready(function () {
+					// We just loaded it, we don't need to load it after this.
+					_.pull($state.js, value);
+
+					checkForEvent();
+				});
 			});
 		}
 	);
