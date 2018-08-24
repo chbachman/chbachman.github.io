468267f32d6b4aabda93d08b101be31af32a13ae
chbachman <cbachman@bachmangroup.com>
2018-08-24 10:03:23 -0500

    Updates

--- background/notepad-save.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env worker */
-importScripts('/vendor/localforage.min.js');
-
-onmessage = function (event) {
-	// var time = Date.now();
-	localforage.setItem('notepad-text', event.data).then(function () {
-		// console.log('Saved in ' + (Date.now() - time) + 'ms');
-	});
-};
