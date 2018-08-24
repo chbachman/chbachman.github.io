a178bc1359339806f047dbe125ce69729b267bde
chbachman <cbachman@bachmangroup.com>
2018-08-24 09:55:19 -0500

    Update Dependencies, Fix build system

--- gulp_tasks/build.js
+++ gulp_tasks/build.js
@@ -1,31 +1,31 @@
-const gulp = require('gulp');
-const filter = require('gulp-filter');
-const useref = require('gulp-useref');
-const rev = require('gulp-rev');
-const revReplace = require('gulp-rev-replace');
-const uglify = require('gulp-uglify');
-const cssnano = require('gulp-cssnano');
-const htmlmin = require('gulp-htmlmin');
-const sourcemaps = require('gulp-sourcemaps');
-const uglifySaveLicense = require('uglify-save-license');
-const inject = require('gulp-inject');
-const ngAnnotate = require('gulp-ng-annotate');
+const gulp = require('gulp')
+const filter = require('gulp-filter')
+const useref = require('gulp-useref')
+const rev = require('gulp-rev')
+const revReplace = require('gulp-rev-replace')
+const uglify = require('gulp-uglify')
+const cssnano = require('gulp-cssnano')
+const htmlmin = require('gulp-htmlmin')
+const sourcemaps = require('gulp-sourcemaps')
+const uglifySaveLicense = require('uglify-save-license')
+const inject = require('gulp-inject')
+const ngAnnotate = require('gulp-ng-annotate')
 
-const conf = require('../conf/gulp.conf');
+const conf = require('../conf/gulp.conf')
 
-gulp.task('build', build);
+gulp.task('build', build)
 
-function build() {
-  const partialsInjectFile = gulp.src(conf.path.tmp('templateCacheHtml.js'), {read: false});
+function build () {
+  const partialsInjectFile = gulp.src(conf.path.tmp('templateCacheHtml.js'), {read: false})
   const partialsInjectOptions = {
     starttag: '<!-- inject:partials -->',
     ignorePath: conf.paths.tmp,
     addRootSlash: false
-  };
+  }
 
-  const htmlFilter = filter(conf.path.tmp('*.html'), {restore: true});
-  const jsFilter = filter([conf.path.tmp('**/*.js'), '!' + conf.path.tmp('vendor/**/*.js')], {restore: true});
-  const cssFilter = filter(conf.path.tmp('**/*.css'), {restore: true});
+  const htmlFilter = filter(conf.path.tmp('*.html'), {restore: true})
+  const jsFilter = filter([conf.path.tmp('**/*.js'), '!' + conf.path.tmp('vendor/**/*.js')], {restore: true})
+  const cssFilter = filter(conf.path.tmp('**/*.css'), {restore: true})
 
   return gulp.src(conf.path.tmp('/index.html'))
     .pipe(inject(partialsInjectFile, partialsInjectOptions))
@@ -33,19 +33,18 @@ function build() {
     .pipe(jsFilter)
     .pipe(sourcemaps.init())
     .pipe(ngAnnotate())
-    .pipe(uglify({preserveComments: uglifySaveLicense})).on('error', conf.errorHandler('Uglify'))
+    .pipe(uglify({output: {comments: 'some'}})).on('error', conf.errorHandler('Uglify'))
     .pipe(rev())
-    .pipe(sourcemaps.write('maps'))
     .pipe(jsFilter.restore)
     .pipe(cssFilter)
     .pipe(sourcemaps.init())
     .pipe(cssnano())
     .pipe(rev())
-    .pipe(sourcemaps.write('maps'))
     .pipe(cssFilter.restore)
     .pipe(revReplace())
+    .pipe(sourcemaps.write('maps'))
     .pipe(htmlFilter)
     .pipe(htmlmin())
     .pipe(htmlFilter.restore)
-    .pipe(gulp.dest(conf.path.dist()));
+    .pipe(gulp.dest(conf.path.dist()))
 }
--- gulp_tasks/inject.js
+++ gulp_tasks/inject.js
@@ -1,30 +1,31 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync');
-const wiredep = require('wiredep').stream;
-const angularFilesort = require('gulp-angular-filesort');
-const gulpInject = require('gulp-inject');
+const gulp = require('gulp')
+const browserSync = require('browser-sync')
+const wiredep = require('wiredep').stream
+const angularFilesort = require('gulp-angular-filesort')
+const gulpInject = require('gulp-inject')
 
-const conf = require('../conf/gulp.conf');
+const conf = require('../conf/gulp.conf')
 
-gulp.task('inject', inject);
+gulp.task('inject', inject)
 
-function inject() {
+function inject () {
   const injectScripts = gulp.src([
     conf.path.tmp('**/*.js'),
     `!${conf.path.tmp('**/*.spec.js')}`,
     `!${conf.path.tmp('vendor/**/*.js')}`,
-		`!${conf.path.tmp('background/**/*.js')}`
+    `!${conf.path.tmp('background/**/*.js')}`,
+    `!${conf.path.tmp('**/*.js.map')}`
   ])
-  .pipe(angularFilesort()).on('error', conf.errorHandler('AngularFilesort'));
+    .pipe(angularFilesort()).on('error', conf.errorHandler('AngularFilesort'))
 
   const injectOptions = {
     ignorePath: [conf.paths.src, conf.paths.tmp],
     addRootSlash: false
-  };
+  }
 
   return gulp.src(conf.path.src('index.html'))
     .pipe(gulpInject(injectScripts, injectOptions))
     .pipe(wiredep(Object.assign({}, conf.wiredep)))
     .pipe(gulp.dest(conf.paths.tmp))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.stream())
 }
